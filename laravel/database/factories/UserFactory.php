3fd5212b71fe9dd26b4f85e21ee8fcc66fc0cf50
nhockizi <nhockizi@gmail.com>
2021-12-08 16:49:42 +0700

    no message

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => 'Admin',
-            'phone' => $this->faker->phoneNumber();
+            'phone' => $this->faker->phoneNumber(),
             'email' => 'admin@gmail.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
