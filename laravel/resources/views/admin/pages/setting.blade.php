7b93430c523781475ff845400ec5d3f59bdb08d2
nhockizi <nhockizi@gmail.com>
2021-12-09 11:36:16 +0700

    no message

--- laravel/resources/views/admin/pages/setting.blade.php
+++ laravel/resources/views/admin/pages/setting.blade.php
@@ -111,46 +111,6 @@
                             @enderror
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-form-label col-3 text-lg-right text-left">
-                            Logo
-                        </label>
-                        <div class="col-9">
-                            <div
-                                style="cursor:pointer; position: relative; width: 100px; height: 100px; line-height: 30px; text-align: center;">
-                                <div class="images-preview preview-logo">
-                                    @if(isset($settings['logo']))
-                                        <img
-                                            src="{{ \App\Libraries\Helpers::getFileUrl($settings['logo']['path'], $settings['logo']['disk'])  }}">
-                                    @else
-                                        <img src="{{asset('images/default.png')}}">
-                                    @endif
-                                </div>
-                                <input id="app_logo" name="logo" type="file"
-                                       style="opacity: 0.0; position: absolute; top:0; left: 0; bottom: 0; right:0; width: 100%; height:100%;"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-form-label col-3 text-lg-right text-left">
-                            Favicon
-                        </label>
-                        <div class="col-9">
-                            <div
-                                style="cursor:pointer; position: relative; width: 100px; height: 100px; line-height: 30px; text-align: center;">
-                                <div class="images-preview preview-favicon">
-                                    @if(isset($settings['favicon']))
-                                        <img
-                                            src="{{ \App\Libraries\Helpers::getFileUrl($settings['favicon']['path'], $settings['favicon']['disk'])  }}">
-                                    @else
-                                        <img src="{{asset('images/default.png')}}">
-                                    @endif
-                                </div>
-                                <input id="app_favicon" name="favicon" type="file"
-                                       style="opacity: 0.0; position: absolute; top:0; left: 0; bottom: 0; right:0; width: 100%; height:100%;"/>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <div class="card-footer text-right">
                     <button type="submit" class="btn btn-success mr-2">Lưu</button>
